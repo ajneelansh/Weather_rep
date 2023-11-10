7795a65cb072633776eae5ebcfccf4c47ef3d467
unknown <ajneelansh@gmail.com>
2023-11-11 01:14:10 +0530

    improvement

---
+++ android/app/src/main/kotlin/com/example/weather_rep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_rep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_rep/pages/home_page.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Weather Report',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage());
+  }
+}
